5594d6bf4c34a67cbbf2ffdb1491ed91bed32cb6
lcamilo <lhenriquegomescamilo@gmail.com>
2019-04-28 14:15:06 -0300

    Ajustando portas

--- src/main.options.ts
+++ src/main.options.ts
@@ -2,7 +2,7 @@ import {NestMicroserviceOptions} from '@nestjs/common/interfaces/microservices/n
 import {MicroserviceOptions} from '@nestjs/common/interfaces/microservices/microservice-configuration.interface';
 import {Transport} from '@nestjs/common/enums/transport.enum';
 
-const port = (process.env.PORT_MICROSERVICE_DATABASE || 3001) as number;
+const port = 3001;
 export const OPTIONS: NestMicroserviceOptions & MicroserviceOptions = {
     transport: Transport.TCP,
     options: {port},
