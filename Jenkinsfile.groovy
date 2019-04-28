98e38d9d413644d7d5693f4976512ddfbeb89929
lcamilo <lhenriquegomescamilo@gmail.com>
2019-04-28 00:35:27 -0300

    Ajustando prota do microservice de database para o ambiente develop

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -41,7 +41,7 @@ podTemplate(
                 ENVIRONMENT = 'development'
                 HELM_DEPLOY_NAME = ENVIRONMENT + "-" + MICROSERVICE_NAME
                 IMAGE_POSFIX = "-RC"
-                NODE_PORT = "30011"
+                NODE_PORT = "30013"
                 echo "Ambiente development"
                 echo "CURRENT BRANCH ${GIT_BRANCH}"
             } else {
